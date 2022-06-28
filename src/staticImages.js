8e3c1fd06053a5919cd0f6b8cb060689ec9ca6bc
kisublee <yigi2021@gmail.com>
2022-06-28 18:05:54 -0400

    Create a static data with static images.

---
+++ src/staticImages.js
@@ -0,0 +1,23 @@
+import mc from "./media/mc.png"
+import pizza from "./media/pizza.png"
+import japanese from "./media/japanese.png"
+import greek from "./media/greek.png"
+import french from "./media/french.png"
+import thai from "./media/thai.png"
+import mexican from "./media/mexican.png"
+import korean from "./media/korean.png"
+
+
+
+const imageOfRestaurants = [
+   {"mc": mc}, 
+   {"pizza": pizza}, 
+   {"japanese": japanese}, 
+   {"greek": greek}, 
+   {"french": french},
+   {"thai": thai},
+   {"mexican": mexican},
+   {"korean": korean},
+]
+
+module.exports = imageOfRestaurants;
