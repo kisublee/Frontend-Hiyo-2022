60b99676b0dd7cc3e1c198af107689956422b196
kisublee <yigi2021@gmail.com>
2022-07-05 02:01:00 -0400

    Create an alert feature for invalid input. Commented out for now.

---
+++ src/utilities/alerts/AlertsForInput.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import Button from '@mui/material/Button';
+import CloseIcon from '@mui/icons-material/Close';
+
+export default function AlertsForInput() {
+
+const [open, setOpen] = React.useState(true);
+
+  return (
+    <Stack sx={{ width: '100%', mt:"0.5vh"}} spacing={2}>
+      <Alert variant="filled" severity="error">
+        Please enter a valid input. Follow examples. 
+      </Alert>
+    </Stack>
+  );
+}
